b4bec71ad20f25d5c557f345d2e28948f9a445f1
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-04-03 16:21:28 -0500

    net: thunderx: nicvf_main: Fix potential NULL pointer dereference
    Add null check on kmalloc() return value in order to prevent
    a null pointer dereference.
    
    Addresses-Coverity-ID: 1467429 ("Dereference null return value")
    Fixes: 37c3347eb247 ("net: thunderx: add ndo_set_rx_mode callback implementation for VF")
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>

--- drivers/net/ethernet/cavium/thunder/nicvf_main.c
+++ drivers/net/ethernet/cavium/thunder/nicvf_main.c
@@ -1999,6 +1999,8 @@ static void nicvf_set_rx_mode(struct net_device *netdev)
 				struct xcast_addr *xaddr;
 
 				mc_list = kmalloc(sizeof(*mc_list), GFP_ATOMIC);
+				if (unlikely(!mc_list))
+					return
 				INIT_LIST_HEAD(&mc_list->list);
 				netdev_hw_addr_list_for_each(ha, &netdev->mc) {
 					xaddr = kmalloc(sizeof(*xaddr),

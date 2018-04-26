44e6a31fa9f9e6355f633c3ecadd69fe53962575
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-04-26 17:03:40 -0500

    tw68-video: fix potential Spectre variant 1
    f->index can be controlled by user-space, hence leading to
    a potential exploitation of the Spectre variant 1 vulnerability.
    
    Smatch warning:
    drivers/media/pci/tw68/tw68-video.c:795 tw68_enum_fmt_vid_cap() warn:
    potential spectre issue 'formats'
    
    Fix this by sanitizing f->index before using it to index
    formats.
    
    Notice that given that speculation windows are large, the policy is
    to kill the speculation on the first load and not worry if it can be
    completed with a dependent load/store [1].
    
    [1] https://marc.info/?l=linux-kernel&m=152449131114778&w=2
    
    Cc: stable@vger.kernel.org
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>

--- drivers/media/pci/tw68/tw68-video.c
+++ drivers/media/pci/tw68/tw68-video.c
@@ -30,6 +30,8 @@
 #include <media/v4l2-event.h>
 #include <media/videobuf2-dma-sg.h>
 
+#include <linux/nospec.h>
+
 #include "tw68.h"
 #include "tw68-reg.h"
 
@@ -788,6 +790,8 @@ static int tw68_enum_fmt_vid_cap(struct file *file, void  *priv,
 {
 	if (f->index >= FORMATS)
 		return -EINVAL;
+
+	f->index = array_index_nospec(f->index, FORMATS);
 
 	strlcpy(f->description, formats[f->index].name,
 		sizeof(f->description));
